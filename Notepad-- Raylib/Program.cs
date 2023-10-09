162b25abe177888a72216346b016029a370f514c
apilatosba <aposdapulos098@outlook.com>
2023-10-09 19:08:03 +0300

    limited fps

--- Notepad-- Raylib/Program.cs
+++ Notepad-- Raylib/Program.cs
@@ -127,6 +127,7 @@ namespace Notepad___Raylib {
 
          Raylib.InitWindow(windowSaveData.size.x, windowSaveData.size.y, "Notepad--");
 
+         Raylib.SetTargetFPS(144); // TODO vsync?
          Raylib.SetWindowIcon(Raylib.LoadImage(Path.Combine(GetImagesDirectory(), "icon4.png")));
          Raylib.SetWindowPosition(windowSaveData.position.x, windowSaveData.position.y);
          Raylib.SetWindowState((windowSaveData.maximized ? ConfigFlags.FLAG_WINDOW_MAXIMIZED : 0)
